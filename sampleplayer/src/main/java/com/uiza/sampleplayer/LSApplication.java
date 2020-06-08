e0a02e79499fc0df47bf30045c54d5d2dcb6e5fe
Nam Nguyen <namdinh@uiza.io>
2020-06-08 15:10:15 +0700

    update sample

--- sampleplayer/src/main/java/com/uiza/sampleplayer/LSApplication.java
+++ sampleplayer/src/main/java/com/uiza/sampleplayer/LSApplication.java
@@ -11,9 +11,9 @@ public class LSApplication extends MultiDexApplication {
 
     public static final String[] urls = new String[]{
             "https://1775190502.rsc.cdn77.org/live/1ffbc4db-8dea-4087-a281-a6c3003cb121/master.m3u8?cm=eyJlbnRpdHlfaWQiOiIxZmZiYzRkYi04ZGVhLTQwODctYTI4MS1hNmMzMDAzY2IxMjEiLCJlbnRpdHlfc291cmNlIjoibGl2ZSIsImFwcF9pZCI6ImI5NjNiNDY1YzM0ZTRmZmI5YTcxOTIyNDQyZWUwZGNhIn0=",
-            "https://1775190502.rsc.cdn77.org/live/1ffbc4db-8dea-4087-a281-a6c3003cb121/manifest.mpd?cm=eyJlbnRpdHlfaWQiOiIxZmZiYzRkYi04ZGVhLTQwODctYTI4MS1hNmMzMDAzY2IxMjEiLCJlbnRpdHlfc291cmNlIjoibGl2ZSIsImFwcF9pZCI6ImI5NjNiNDY1YzM0ZTRmZmI5YTcxOTIyNDQyZWUwZGNhIn0=",
             "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
-            "https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd"};
+            "https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd",
+            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"};
 
     public static final String thumbnailUrl = "https://1955897154.rsc.cdn77.org/6fd7eafc8e6c441ea3f14c528f7266e6-static/2020/05/27/94a04fa4-07e2-43e5-9b86-d65f01bca611/thumbnail-10-8-720.jpeg";
 
