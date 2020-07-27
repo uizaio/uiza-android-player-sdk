4042547568b3966edcf491e9064c34a41d3c6067
Nam Nguyen <namnd.bka@gmail.com>
2020-07-27 10:22:27 +0700

    fix live status color

--- sampleplayer/src/main/java/com/uiza/sampleplayer/LSApplication.java
+++ sampleplayer/src/main/java/com/uiza/sampleplayer/LSApplication.java
@@ -11,8 +11,8 @@ import timber.log.Timber;
 public class LSApplication extends MultiDexApplication {
 
     public static final String[] urls = new String[]{
-            "https://hls.ted.com/talks/2639.m3u8?preroll=Thousands",
             "https://uz-test2live.uizacdn.net/865be795-836e-4f08-bbb6-34808374dced.smil/playlist.m3u8?cm=eyJlbnRpdHlfaWQiOiI4NjViZTc5NS04MzZlLTRmMDgtYmJiNi0zNDgwODM3NGRjZWQiLCJlbnRpdHlfc291cmNlIjoibGl2ZSIsImFwcF9pZCI6Ijk2NTU4YWI0YTZiMTRlOTA5ZWVkOThjMWNlNTBkNWVmIn0=",
+            "https://hls.ted.com/talks/2639.m3u8?preroll=Thousands",
             "https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd",
             "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
             "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"};
@@ -30,6 +30,6 @@ public class LSApplication extends MultiDexApplication {
         }
         UZPlayer.init(this, true);
         UZApi.init(this,"SamplePlayer", BuildConfig.VERSION_NAME);
-        UZData.getInstance().setUrlIMAAd(VAST_SAMPLE_URL);
+//        UZData.getInstance().setUrlIMAAd(VAST_SAMPLE_URL);
     }
 }
